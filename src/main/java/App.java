cc3efdb0e3df434f5b5c834a6c1c1db7365e99aa
daniescobarx <daniela.mollo@sptech.school>
2024-04-09 13:14:27 -0300

    feat: class MemoriaRam funcionando

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
+import componentes.MemoriaRam;
 import componentes.Sistema;
 public class App {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
-
+        MemoriaRam memoriaram = new MemoriaRam();
         sistema.exibirInformacoesSistema();
+        memoriaram.exibirInformacoesMemoria();
+
     }
 }
 
--- src/main/java/componentes/MemoriaRam.java
+++ src/main/java/componentes/MemoriaRam.java
@@ -2,14 +2,26 @@ package componentes;
 import com.github.britooo.looca.api.core.Looca;
 import com.github.britooo.looca.api.group.memoria.Memoria;
 public class MemoriaRam {
-    private Double memoriaTotal;
-    private Double memoriaEmUso;
-    private Double memoriaDisponivel;
-    public void captarMemoria() {
+    private Long memoriaTotal;
+    private Long memoriaEmUso;
+    private Long memoriaDisponivel;
+    public void captarInformacoesMemoria() {
         Looca looca = new Looca();
         Memoria memoria = looca.getMemoria();
 
+        memoriaTotal = memoria.getTotal();
+        memoriaEmUso = memoria.getEmUso();
+        memoriaDisponivel = memoria.getDisponivel();
     }
+
+    public void exibirInformacoesMemoria() {
+        captarInformacoesMemoria();
+        System.out.println("\n------Informações da Memória RAM------");
+        System.out.println("Memória Total: " + memoriaTotal + " GB");
+        System.out.println("Memória em Uso: " + memoriaEmUso + " GB");
+        System.out.println("Memória Disponível: " + memoriaDisponivel + " GB");
+    }
+
 }
 
 
