5a13bd7175d7872cf8b40879eb9ac78fb3316d76
daniescobarx <daniela.mollo@sptech.school>
2024-04-10 09:36:56 -0300

    Feat: Add métodos shutdown() e restart() para executar comandos de desligamento e reinicialização do sistema

--- src/main/java/SystemCommandExecutor.java
+++ src/main/java/SystemCommandExecutor.java
@@ -1,19 +1,32 @@
 import java.io.IOException;
 
+
 public class SystemCommandExecutor {
-    public static void main(String[] args) {
+    public static void shutdown(){
+        executeCommand("shutdown", "-s");
+    }
+    public static void restart(){
+        executeCommand("shutdown", "-r");
+    }
+
+    private static void executeCommand(String... command) {
         try{
-            ProcessBuilder processBuilder = new ProcessBuilder("shutdown", "-s");
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
             Process process = processBuilder.start();
             Integer exitCode = process.waitFor();
-
             if (exitCode == 0){
-                System.out.println("O sistema foi desligado com sucesso.");
+                System.out.println("Comando executado com sucesso");
             }else {
-                System.out.println("Falha ao desligar o sistema.\nCódigo de saída: "+exitCode);
+                System.out.println("Falha ao executar o comando.\nCódigo de saída: "+exitCode);
             }
         }catch (IOException | InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+
+    public static void main(String[] args) {
+        SystemCommandExecutor.shutdown();
+        SystemCommandExecutor.restart();
     }
 }
