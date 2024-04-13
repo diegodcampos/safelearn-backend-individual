ad1c4c93e567c507e8808ba73875429abf8d5eea
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-04-13 18:03:08 -0300

    feat: Classe 'MaquinaDao' recebe a classe 'MemoriaRam' em sua função e a adiociona em query sql

--- src/main/java/dao/MaquinaDao.java
+++ src/main/java/dao/MaquinaDao.java
@@ -1,17 +1,19 @@
 package dao;
 
+import componentes.MemoriaRam;
 import componentes.Sistema;
 import componentes.UsoDisco;
 import componentes.UsoProcessador;
 import conexao.Conexao;
+import oshi.util.Memoizer;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MaquinaDao {
-    public void inserirDadosMaquina(UsoProcessador processador, Sistema sistema, UsoDisco disco) {
-        String sql = "INSERT INTO maquina (idProcessador, modeloProcessador, fabricanteProcessador, identificador, microarquitetura, frequencia, cpusFisicas, cpusLogicas, so, arquitetura, fabricanteSo, modeloDisco, tamanhoDisco, modeloDisco2, tamanhoDisco2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    public void inserirDadosMaquina(UsoProcessador processador, Sistema sistema, UsoDisco disco, MemoriaRam memoriaRam) {
+        String sql = "INSERT INTO maquina (idProcessador, modeloProcessador, fabricanteProcessador, identificador, microarquitetura, frequencia, cpusFisicas, cpusLogicas, usoProcessador, so, arquitetura, fabricanteSo, memoriaRamTotal, memoriaRamEmUso, memoriaRamDisponivel, modeloDisco, tamanhoDisco, modeloDisco2, tamanhoDisco2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         PreparedStatement ps = null;
 
@@ -26,9 +28,13 @@ public class MaquinaDao {
             ps.setLong(6, processador.getFrequencia());
             ps.setInt(7, processador.getNumeroCpusFisicas());
             ps.setInt(8, processador.getNumeroCpusLogicas());
-            ps.setString(9, sistema.getSistemaOperacional());
-            ps.setInt(10, sistema.getArquitetura());
-            ps.setString(11, sistema.getFabricante());
+            ps.setDouble(9, processador.getUso());
+            ps.setString(10, sistema.getSistemaOperacional());
+            ps.setInt(11, sistema.getArquitetura());
+            ps.setString(12, sistema.getFabricante());
+            ps.setDouble(13, memoriaRam.getMemoriaTotal());
+            ps.setDouble(14, memoriaRam.getMemoriaEmUso());
+            ps.setDouble(15, memoriaRam.getMemoriaDisponivel());
 
             List<String> modelosDisco = disco.getModelosDisco();
             List<Long> tamanhosDisco = disco.getTamanhosDisco();
@@ -37,8 +43,8 @@ public class MaquinaDao {
                 throw new IllegalArgumentException("O número de modelos de disco não corresponde ao número de tamanhos.");
             }
 
-            Integer parameterIndex = 12;
-            Integer parameterIndex2 = 13;
+            Integer parameterIndex = 16;
+            Integer parameterIndex2 = 17;
             for (int i = 0; i < modelosDisco.size(); i++) {
                 ps.setString(parameterIndex, modelosDisco.get(i));
                 ps.setLong(parameterIndex2, tamanhosDisco.get(i));
