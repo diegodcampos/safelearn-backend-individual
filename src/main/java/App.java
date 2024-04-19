e652709cc1059cf4e17f8d8df871238f638dcee3
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-04-19 11:10:36 -0300

    feat: Adição de método que insere os dados da máquina no BD automaticamente

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,15 @@
 
-import componentes.GrupoJanelas;
-import componentes.MemoriaRam;
-import componentes.UsoDisco;
-import componentes.UsoProcessador;
+import componentes.*;
+import dao.MaquinaDao;
+import setup.InserirRegistros;
 import setup.Logar;
 
 public class App {
     public static void main(String[] args) {
-        UsoProcessador processador = new UsoProcessador();
-        MemoriaRam memoriaram = new MemoriaRam();
-        UsoDisco usoDisco = new UsoDisco();
-        GrupoJanelas grupoJanelas = new GrupoJanelas();
-
-        System.out.println(grupoJanelas.toString());
-        System.out.println(processador.toStringSimplified());
-        System.out.println(memoriaram.toString());
-        usoDisco.exibirInformacoesDeDiscos();
-
         Logar logar = new Logar();
-        logar.logar();
 
+        logar.logar();
+        InserirRegistros inserirRegistros = new InserirRegistros();
     }
 }
 
--- src/main/java/setup/InserirRegistros.java
+++ src/main/java/setup/InserirRegistros.java
@@ -1,2 +1,33 @@
-package setup;public class InserirRegistros {
+package setup;
+
+import componentes.MemoriaRam;
+import componentes.Sistema;
+import componentes.UsoDisco;
+import componentes.UsoProcessador;
+import dao.MaquinaDao;
+
+public class InserirRegistros {
+    public InserirRegistros() {
+        inserirRegistros();
+    }
+
+    public void inserirRegistros() {
+        UsoProcessador processador = new UsoProcessador();
+        Sistema sistema = new Sistema();
+        UsoDisco usoDisco = new UsoDisco();
+        MemoriaRam memoriaram = new MemoriaRam();
+
+        Boolean possuiRegistro;
+        possuiRegistro = new MaquinaDao().verificarRegistro(processador);
+
+        if(!possuiRegistro) {
+            new MaquinaDao().inserirDadosMaquina(processador, sistema, usoDisco, memoriaram);
+            System.out.println("Registrado com sucesso!");
+            String stringHardware = processador.toStringSimplified() + sistema.toString() + memoriaram.toString();
+            System.out.println("Aqui estão algumas informações básicas sobre sua máquina:");
+            System.out.println(stringHardware);
+        } else {
+            System.out.println("A máquina está sendo monitorada.");
+        }
+    }
 }
