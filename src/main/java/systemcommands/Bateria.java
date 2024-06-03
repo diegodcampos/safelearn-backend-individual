83b562d50b1b4200f561a807258bbdf1e77cccbe
daniescobarx <daniela.mollo@sptech.school>
2024-06-02 21:30:57 -0300

    refactor: ajuste na bateria

--- src/main/java/systemcommands/Bateria.java
+++ src/main/java/systemcommands/Bateria.java
@@ -5,14 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Bateria {
+    private int cargaRestante;
+    private int statusBateria;
+
     public void verificarBateria() {
         try {
-            Process powerShellProcess = Runtime.getRuntime().exec("powershell.exe Get-WmiObject Win32_Battery | Select-Object EstimatedChargeRemaining, Status, BatteryStatus | Format-Table -Property @{Label='Carga Restante %';Expression={$_.EstimatedChargeRemaining}}, @{Label='Status';Expression={$_.Status}}, @{Label='Status da Bateria';Expression={$_.BatteryStatus}} -AutoSize -Wrap");
+            Process powerShellProcess = Runtime.getRuntime().exec(
+                    "powershell.exe Get-WmiObject Win32_Battery | Select-Object EstimatedChargeRemaining, Status, BatteryStatus | Format-Table -Property @{Label='Carga Restante %';Expression={$_.EstimatedChargeRemaining}}, @{Label='Status';Expression={$_.Status}}, @{Label='Status da Bateria';Expression={$_.BatteryStatus}} -AutoSize -Wrap"
+            );
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
+                if (line.contains("Carga Restante %")) {
+                    reader.readLine();
+                } else if (line.trim().length() > 0) {
+                    String[] parts = line.trim().split("\\s+");
+                    if (parts.length >= 3) {
+                        try {
+                            cargaRestante = Integer.parseInt(parts[0]);
+                            statusBateria = Integer.parseInt(parts[2]);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Erro ao analisar os valores: " + e.getMessage());
+                        }
+                    }
+                }
             }
             reader.close();
 
@@ -24,7 +42,26 @@ public class Bateria {
             System.out.println("O processo foi interrompido: " + e.getMessage());
             e.printStackTrace();
         } finally {
-            System.out.println("status da bateria '1': desconectada da tomada\nstatus da bateria '2': conectada na tomada");
+            System.out.println("Status da bateria '1': desconectada da tomada\nStatus da bateria '2': conectada na tomada");
+            System.out.println(" ");
+
         }
+    }
+
+    public int getCargaRestante() {
+        return cargaRestante;
+    }
+
+    public int getStatusBateria() {
+        return statusBateria;
+    }
+
+    public static void main(String[] args) {
+        Bateria bateria = new Bateria();
+        bateria.verificarBateria();
+        int carga = bateria.getCargaRestante();
+        int status = bateria.getStatusBateria();
+        System.out.println("A carga restante da bateria é: " + carga + "%");
+        System.out.println("O status da bateria é: " + status);
     }
 }
