61b8760247299651a6669ffd8ff3d34b9f3fed11
Diego de Campos <diego.arruda@sptech.school>
2024-06-17 12:43:11 -0300

    subindo a versao final do jar individual

--- src/main/java/setup/Logar.java
+++ src/main/java/setup/Logar.java
@@ -21,8 +21,9 @@ public class Logar {
         do {
             System.out.println("Selecione uma opção:");
             System.out.println("1. Informações sobre componentes de um computador");
-            System.out.println("2. Fazer login");
-            System.out.println("3. Sair");
+            System.out.println("2. História do Disco Rígido");
+            System.out.println("3. Fazer login");
+            System.out.println("4. Sair");
 
             opcao = scanner.nextInt();
             scanner.nextLine();
@@ -32,9 +33,12 @@ public class Logar {
                     exibirInformacoesComponentes();
                     break;
                 case 2:
-                    fazerLogin(scanner);
+                    exibirHistoriaDiscoRigido();
                     break;
                 case 3:
+                    fazerLogin(scanner);
+                    break;
+                case 4:
                     System.out.println("Saindo...");
                     System.exit(0);
                     break;
@@ -42,7 +46,7 @@ public class Logar {
                     System.out.println("Opção inválida. Tente novamente.");
                     break;
             }
-        } while (opcao != 2);
+        } while (opcao != 3);
     }
 
     private void exibirInformacoesComponentes() {
@@ -69,8 +73,26 @@ public class Logar {
         System.out.println(informacoes);
     }
 
+    private void exibirHistoriaDiscoRigido() {
+        String historia = """
+        \n-----------------------------------------------------------------------------------------------------------
+        História do Disco Rígido:
+        O disco rígido, ou HDD (Hard Disk Drive), é um dispositivo de armazenamento de dados introduzido pela primeira vez pela IBM em 1956. O primeiro modelo, chamado IBM 350, fazia parte do sistema IBM 305 RAMAC (Random Access Method of Accounting and Control). Ele tinha uma capacidade de cerca de 5 MB, usando cinquenta discos de 24 polegadas, cada um dos quais armazenava 100.000 bytes.
+        
+        Nos anos 1980, a tecnologia de discos rígidos começou a se tornar mais acessível e compacta, com empresas como Seagate e Western Digital lançando unidades para PCs. O tamanho padrão dos discos passou de 8 polegadas para 5,25 e finalmente 3,5 polegadas para desktops e 2,5 polegadas para laptops.
+        
+        Na década de 1990, os HDDs continuaram a evoluir, com aumentos significativos na capacidade e na velocidade. A introdução de interfaces como IDE (Integrated Drive Electronics) e posteriormente SATA (Serial ATA) melhorou a taxa de transferência de dados e a facilidade de conexão.
+        
+        Com o advento dos SSDs (Solid State Drives) no século 21, os HDDs começaram a enfrentar concorrência séria. No entanto, devido ao seu custo por gigabyte mais baixo, os discos rígidos ainda são amplamente utilizados para armazenamento de dados em massa.
+        
+        Os HDDs modernos podem armazenar vários terabytes de dados e possuem tecnologias avançadas como cache grande, correção de erros e algoritmos de otimização de leitura/escrita. Eles continuam a ser uma escolha popular para armazenamento de grandes volumes de dados, como backups, sistemas de vigilância e servidores.
+        -----------------------------------------------------------------------------------------------------------
+        """;
 
-    private void fazerLogin (Scanner scanner){
+        System.out.println(historia);
+    }
+
+    private void fazerLogin(Scanner scanner) {
         Boolean autenticado = false;
 
         do {
